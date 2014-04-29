0cff490feaaab7bdbf8753a5e4f75ecfd6985755
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-04-29 20:37:07 +0100

    rebrand

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 var test = require("tape"),
-    config = require("../lib/load-config.js");
+    config = require("../lib/config-master.js");
 
 test("main", function(t){
     var c = config("test/config1.json", "test/config2.json", "package.json:config");
