a13cd3e7e86304bf6c8d6017c8241b2d7657ae77
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-04-29 15:09:01 +0100

    [] to arguments

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var test = require("tape"),
     config = require("../lib/load-config.js");
 
 test("main", function(t){
-    var c = config([ "test/config1.json", "test/config2.json", "package.json:config" ]);
+    var c = config("test/config1.json", "test/config2.json", "package.json:config");
     t.deepEqual(c, {
         one: "einen",
         two: "zwei",
