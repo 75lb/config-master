3d70f1d46fca9b3f7fa472992a094ef3079290d4
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-04-29 14:36:48 +0100

    initial

---
+++ test/test.js
@@ -0,0 +1,13 @@
+var test = require("tape"),
+    config = require("../lib/config.js");
+
+test("main", function(t){
+    var c = config([ "test/config1.json", "test/config2.json", "package.json:config" ]);
+    t.deepEqual(c, {
+        one: "einen",
+        two: "zwei",
+        three: 3,
+        four: "package"
+    });
+    t.end();
+});
