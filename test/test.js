b92166ec2802263091bc8840e92329ea36b598a1
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-05-06 15:39:01 +0200

    handles missing property in required json file

--- test/test.js
+++ test/test.js
@@ -1,13 +1,23 @@
 var test = require("tape"),
     config = require("../lib/config-master.js");
 
-test("main", function(t){
-    var c = config("test/config1.json", "test/config2.json", "package.json:config");
+test("merges correctly", function(t){
+    var c = config("test/config1.json", "test/config2.json", "test/package.json:config");
     t.deepEqual(c, {
         one: "einen",
         two: "zwei",
         three: 3,
         four: "package"
+    });
+    t.end();
+});
+
+test("missing package.json property", function(t){
+    var c = config("test/config1.json", "test/config2.json", "test/package2.json:config");
+    t.deepEqual(c, {
+        "one": 1,
+        "two": "zwei",
+        "three": 3
     });
     t.end();
 });
