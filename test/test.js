8b12c7d2f0e3598a19fd8bbaf7c74217c2c8816c
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-11-11 10:46:58 +0000

    rewrite test

--- test/test.js
+++ test/test.js
@@ -1,31 +1,13 @@
 var test = require('tape')
-var config = require('../lib/config-master.js')
-var path = require('path')
+var loadConfig = require('../lib/config-master.js')
 
-var path = {
-  one: path.resolve('test', 'config1.json'),
-  two: path.resolve('test', 'config2.json'),
-  three: { jsonPath: path.resolve('test', 'package.json'), configProperty: 'config' },
-  four: { jsonPath: path.resolve('test', 'package2.json'), configProperty: 'config' }
-}
-
-test('merges correctly', function (t) {
-  var c = config(path.one, path.two, path.three)
-  t.deepEqual(c, {
-    one: 'einen',
-    two: 'zwei',
+test('new API', function(t){
+  var config = loadConfig('test-app', { startFrom: __dirname + '/fixture/one/two' })
+  t.deepEqual(config, {
+    one: 1,
+    two: 2,
     three: 3,
     four: 'package'
-  })
-  t.end()
-})
-
-test('missing package.json property', function (t) {
-  var c = config(path.one, path.two, path.four)
-  t.deepEqual(c, {
-    'one': 1,
-    'two': 'zwei',
-    'three': 3
   })
   t.end()
 })
