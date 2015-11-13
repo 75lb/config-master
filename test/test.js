6d7e01f0a7a91f3349f73a1f6900ae0aea0b4165
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-11-13 11:50:38 +0000

    lint

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@
 var test = require('tape')
 var loadConfig = require('../')
 
-test('new API', function(t){
+test('new API', function (t) {
   var config = loadConfig('test-app', { startFrom: __dirname + '/fixture/one/two' })
   t.deepEqual(config, {
     one: 1,
