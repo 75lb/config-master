5a894fd9557439b4c959c9c6e9ee779031df1c40
Lloyd Brookes <lloyd.brookes@gmail.com>
2016-01-02 22:57:46 +0000

    upgrade deps

--- es5/config-master.js
+++ es5/config-master.js
@@ -2,7 +2,7 @@
 
 var _marked = [configsInTree, packageConfigsInTree].map(regeneratorRuntime.mark);
 
-function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) arr2[i] = arr[i]; return arr2; } else { return Array.from(arr); } }
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
 var path = require('path');
 var walkBack = require('walk-back');
