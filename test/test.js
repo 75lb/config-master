343e28f8ea2325b13b09ff1f5b10d4bfaed0b4b8
Lloyd Brookes <lloyd.brookes@gmail.com>
2017-02-09 20:36:02 +0000

    upgrade deps

--- test/test.js
+++ test/test.js
@@ -1,11 +1,13 @@
 'use strict'
-const test = require('test-runner')
+const TestRunner = require('test-runner')
 const loadConfig = require('../')
 const a = require('assert')
 
-test('new API', function () {
+const runner = new TestRunner()
+
+runner.test('new API', function () {
   const config = loadConfig('test-app', { startFrom: __dirname + '/fixture/one/two' })
-  a.deepEqual(config, {
+  a.deepStrictEqual(config, {
     one: 1,
     two: 2,
     three: 3,
